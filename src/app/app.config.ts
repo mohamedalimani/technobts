679588980b9dac32fc1b2d2d1cbe856df99c38b1
mohamedalimani <dali.mani652@gmail.com>
2026-06-08 05:25:08 +0100

    page doesn't start from top on loading a page

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(withFetch()), provideRouter(routes, withComponentInputBinding())]
+  providers: [provideHttpClient(withFetch()), 
+    provideRouter(routes, 
+      withInMemoryScrolling({scrollPositionRestoration:'top'}),
+      withComponentInputBinding())]
 };
