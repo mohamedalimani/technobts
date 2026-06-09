3f3952c2f6c9a0bbc2ab9cea765e592177982737
mohamedalimani <dali.mani652@gmail.com>
2026-06-09 05:10:33 +0100

    create button to display all description of our solutions in the dallage page (display first line only on init)

--- src/app/speciality/dallage/dallage.component.ts
+++ src/app/speciality/dallage/dallage.component.ts
@@ -10,4 +10,14 @@ export class DallageComponent {
   dallageImg=['2.jpeg', '3.jpeg', '4.jpeg', '5.jpeg', '6.jpeg'];
   cosinusImg=['1.jpeg', '2.jpeg', '3.jpeg'];
   traitImg=['1.jpeg', '2.jpeg'];
+
+  toggleText(event:MouseEvent){
+    const btn=event.target as HTMLElement;
+    const p= btn.previousElementSibling as HTMLElement;
+    const isCollapsed= p.classList.contains('collapsed');
+    p.classList.toggle('collapsed', !isCollapsed);
+    p.classList.toggle('expanded', isCollapsed);
+    btn.textContent=isCollapsed ? 'voir moins':'voir plus';
+  }
+
 }
