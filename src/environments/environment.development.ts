dfb28a111acd8a57a61644a2f3d45250d080e447
mohamedalimani <dali.mani652@gmail.com>
2026-06-11 04:43:20 +0100

    update website logo

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,7 @@
-export const environment = {};
+export const environment = {
+    production:false,
+    EMAILJS_SERVICE_ID:'service_978p7lq',
+    EMAILJS_TEMPLATE_ID:'template_d2qd8ql',
+    EMAILJS_PUBLIC_KEY:'-vZCcyIAseI9b1-3w'
+};
+
